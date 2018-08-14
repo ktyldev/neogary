0852a32ce77387f3f6e2ac7e7d6e5696a85291e5
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-08-14 14:58:17 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace neogary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
