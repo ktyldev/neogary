0ddc3da7e00ec3fde35c2a976d6adae65325d099
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-08-14 18:06:33 +0100

    Factor out config object

--- Bot.cs
+++ Bot.cs
@@ -9,52 +9,17 @@ namespace neogary
     {
         private const string CONFIG_PATH = "config.txt";
 
-        private string _token;
-        private string _owner;
+        private Config _config;
 
         public async Task MainAsync(string[] args)
         {
             Console.WriteLine("bot alive");
 
-            ReadConfig();
+            _config = new Config();
+            Console.WriteLine(_config.Token);
+            Console.WriteLine(_config.Owner);
 
             await Task.Delay(-1);
-        }
-
-        private void ReadConfig()
-        {
-            if (!File.Exists(CONFIG_PATH))
-            {
-                Console.WriteLine("No config found. Creating one...");
-                File.WriteAllLines(CONFIG_PATH, new [] 
-                { 
-                    "token=",
-                    "owner="
-                });
-
-                Console.WriteLine("Please fill in the config and run again :)");
-                Environment.Exit(1);
-            }
-
-            string[] lines = File.ReadAllLines(CONFIG_PATH);            
-            _token = GetConfigValue(lines, "token");
-            _owner = GetConfigValue(lines, "owner");
-
-            if (String.IsNullOrEmpty(_token) || String.IsNullOrEmpty(_owner))
-            {
-                Console.WriteLine("Please fill in the config and run again :)");
-                Environment.Exit(1);
-            }
-
-            Console.WriteLine(_token);
-            Console.WriteLine(_owner);
-        }
-
-        private string GetConfigValue(string[] lines, string key)
-        {
-            return lines
-                .Single(l => l.StartsWith(key))
-                .Split('=')[1];
         }
     }
 }
---
+++ Config.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace neogary
+{
+    public class Config
+    {
+        private const string CONFIG_PATH = "config.txt";
+
+        public string Token { get; private set; }
+        public string Owner { get; private set; }
+
+        public Config()
+        {
+            if (!File.Exists(CONFIG_PATH))
+            {
+                Console.WriteLine("No config found. Creating one...");
+                File.WriteAllLines(CONFIG_PATH, new [] 
+                { 
+                    "token=",
+                    "owner="
+                });
+
+                Console.WriteLine("Please fill in the config and run again :)");
+                Environment.Exit(1);
+            }
+
+            string[] lines = File.ReadAllLines(CONFIG_PATH);            
+            Token = GetConfigValue(lines, "token");
+            Owner = GetConfigValue(lines, "owner");
+
+            if (String.IsNullOrEmpty(Token) || String.IsNullOrEmpty(Owner))
+            {
+                Console.WriteLine("Please fill in the config and run again :)");
+                Environment.Exit(1);
+            }
+        }
+
+        private string GetConfigValue(string[] lines, string key)
+        {
+            return lines
+                .Single(l => l.StartsWith(key))
+                .Split('=')[1];
+        }
+    }
+}
+
+
