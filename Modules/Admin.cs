90ceea213ed38aab8fa2cb629da4d080c8ab9588
ClinkMakesGames <32272073+ClinkMakesGames@users.noreply.github.com>
2018-08-20 16:18:43 +0100

    Prettified help command

--- Modules/Admin.cs
+++ Modules/Admin.cs
@@ -1,3 +1,4 @@
+using Discord;
 using Discord.Commands;
 using System;
 using System.Threading.Tasks;
@@ -32,14 +33,19 @@ namespace neogary
                 "botcommand",
                 "1=1",
                 r => result += String.Format(
-                    "`{0}`\n{1}\n",
+                    "`{0}` | {1}\n",
                     r.GetString(1),
                     r.GetString(2)));
+            
+            var builder = new EmbedBuilder()
+                .WithTitle("Help")
+                .WithDescription(result)
+                .WithCurrentTimestamp();
 
             var user = Context.User;
             var dmChannel = await user.GetOrCreateDMChannelAsync();
 
-            await dmChannel.SendMessageAsync(result);
+            await dmChannel.SendMessageAsync("", false, builder);
         }
     }
 }
